0b60ceae815f023a22a89a69edc5e67e0f5b3c11
Abdel Djalil Chafai <djalilchafai9@gmail.com>
2025-11-22 02:47:55 +0000

    Add files via upload

---
+++ File_Organizer.py
@@ -0,0 +1,22 @@
+#give it a folder 
+#it sorts files into subfolders: 
+    #images 
+    #videos
+    #PDFs 
+    #documents
+    #Others
+
+#section to upload a file
+#section to process the file and indentify type
+#section to move said file to that subfolder 
+
+#Thinking model: 
+#Ask user for folder path
+#look at all files in that folder 
+#detect file extension
+#Decide which folder it belongs to 
+#Move the file there
+#Handle errors 
+
+
+
